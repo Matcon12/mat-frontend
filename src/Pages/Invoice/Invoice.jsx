a3b5b1f8145b9ac3a09bad6e57e8680011b1152d
Matcon12 <matcon545@gmail.com>
2024-05-24 17:01:46 +0530

    made changes in the invoice page

--- src/Pages/Invoice/Invoice.jsx
+++ src/Pages/Invoice/Invoice.jsx
@@ -1,8 +1,10 @@
-import React, { useState } from "react"
+import { useState } from "react"
 import "./Invoice.css"
 
 export default function Invoice() {
   const [customerId, setCustomerId] = useState("")
+  const [consigneeName, setConsigneeName] = useState("");
+  const [newConsigneeName, setNewConsigneeName] = useState("")
   const [poNo, setPoNo] = useState("")
   const [totalEntries, setTotalEntries] = useState()
   const [entries, setEntries] = useState([])
@@ -10,7 +12,14 @@ export default function Invoice() {
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    console.log(entries) // Process the entries as needed
+    const formData = {
+      customerId,
+      consigneeName,
+      newConsigneeName,
+      poNo,
+      items: entries,
+    }
+    console.log(formData)
   }
 
   const handleChange = (index, e) => {
@@ -38,8 +47,18 @@ export default function Invoice() {
   return (
     <div className="invoice-generation-container">
       <h1>Invoice Generation</h1>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit} autoComplete="off">
         <div className="invoice-input-container">
+          <div>
+            <input
+              type="text"
+              required={true}
+              name="poNo"
+              value={poNo}
+              onChange={(e) => setPoNo(e.target.value)}
+            />
+            <label alt="Enter the PO No" placeholder="PO No"></label>
+          </div>
           <div>
             <input
               type="text"
@@ -57,17 +76,33 @@ export default function Invoice() {
             <input
               type="text"
               required={true}
-              name="poNo"
-              value={poNo}
-              onChange={(e) => setPoNo(e.target.value)}
+              name="consigneeName"
+              value={consigneeName}
+              onChange={(e) => setConsigneeName(e.target.value)}
             />
-            <label alt="Enter the PO No" placeholder="PO No"></label>
+            <label
+              alt="Enter the Consignee Name"
+              placeholder="Consignee Name"
+            ></label>
           </div>
           <div>
             <input
               type="text"
-              required={true}
+              name="newConsigneeName"
+              required={false}
+              value={newConsigneeName}
+              onChange={(e) => setNewConsigneeName(e.target.value)}
+            />
+            <label
+              alt="Enter the new Consignee Name"
+              placeholder="New Consignee Name(if required)"
+            ></label>
+          </div>
+          <div>
+            <input
+              type="text"
               name="totalEntries"
+              required={true}
               value={totalEntries}
               onChange={(e) => setTotalEntries(e.target.value)}
             />
@@ -88,7 +123,7 @@ export default function Invoice() {
           {show && (
             <div className="invoice-input-complete-container">
               {entries.map((entry, index) => (
-                <>
+                <div key={index}>
                   <p>Entry {index + 1}</p>
                   <div key={index} className="invoice-form-input-container">
                     <div>
@@ -118,7 +153,7 @@ export default function Invoice() {
                       ></label>
                     </div>
                   </div>
-                </>
+                </div>
               ))}
               <div className="submit-button">
                 <button type="submit">Submit</button>
